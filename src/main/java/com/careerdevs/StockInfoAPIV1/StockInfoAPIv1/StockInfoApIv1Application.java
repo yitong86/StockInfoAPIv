5db9b7ea11b0f659039beafc3c1306332bc0d529
yitong86 <sophiajiang86@hotmail.com>
2022-05-12 19:09:32 -0400

    Initial commit

---
+++ src/main/java/com/careerdevs/StockInfoAPIV1/StockInfoAPIv1/StockInfoApIv1Application.java
@@ -0,0 +1,13 @@
+package com.careerdevs.StockInfoAPIV1.StockInfoAPIv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockInfoApIv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockInfoApIv1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/careerdevs/StockInfoAPIV1/StockInfoAPIv1/StockInfoApIv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs.StockInfoAPIV1.StockInfoAPIv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockInfoApIv1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
